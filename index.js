2ac9e8bb5dd120015625b41e6c2f3e5856516f1d
shirish <shirish101k@gmail.com>
2022-06-02 14:04:11 -0500

    tempSave, added startDate, endDate as args

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet } = require('./lib/update')
 
 ;(async () => {
-  const transactions = await fetchTransactions()
+  const transactions = await fetchTransactions('2021-01-01') // read this startDate from json config.
   const updates = transformTransactionsToUpdates(transactions)
   updateSheet(updates)
 })()
