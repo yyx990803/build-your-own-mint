7aba4228bfcc5a112272aff984183c2f92799bd1
shirish <shirish101k@gmail.com>
2022-06-06 13:35:58 -0500

    save audit results to a new tab in gSheets and source dates from env file

--- index.js
+++ index.js
@@ -5,9 +5,15 @@ const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet, getSheetValues, getNamedRanges } = require('./lib/sheetsCrud')
 const { runAudit } = require('./lib/audit')
 
+const transactionsStartDate = process.env.TRANSACTIONS_START_DATE
+const transactionsEndDate = process.env.TRANSACTIONS_END_DATE
+
+const auditStartDate = process.env.AUDIT_START_DATE
+const auditEndDate = process.env.AUDIT_END_DATE
+
 ;(async () => {
-  const transactions = await fetchTransactions('2021-01-01') // TO DO: read this startDate from config.
+  const transactions = await fetchTransactions(transactionsStartDate, transactionsEndDate)
   const updates = transformTransactionsToUpdates(transactions)
   updateSheet(updates)
-  await runAudit('2022-01-01') // TO DO: get this date from config
+  await runAudit(auditStartDate, auditEndDate)
 })()
