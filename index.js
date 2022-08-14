4f434e7cd060a81cc977ff7dbadc64c57e3a4a2a
shirish <shirish101k@gmail.com>
2022-08-14 17:14:28 -0500

    finished venmo integration

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { fetchTransactions } = require('./lib/fetch')
 const { transformTransactionsToUpdates } = require('./lib/transform')
 const { updateSheet, getSheetValues, getNamedRanges } = require('./lib/sheetsCrud')
 const { runAudit } = require('./lib/audit')
-const { getAllTransactions } = require('./lib/venmoClient')
+const { getAllVenmoTransactions } = require('./lib/venmoClient')
 
 const transactionsStartDate = process.env.TRANSACTIONS_START_DATE
 const transactionsEndDate = process.env.TRANSACTIONS_END_DATE
@@ -13,19 +13,10 @@ const auditStartDate = process.env.AUDIT_START_DATE
 const auditEndDate = process.env.AUDIT_END_DATE
 
 ;(async () => {
-  // const transactions = await fetchTransactions(transactionsStartDate, transactionsEndDate)
-  // const updates = transformTransactionsToUpdates(transactions)
-  // await updateSheet([{ range: 'testApi!A1:A3', values: [[6],[6],[9]]}])
-  // await updateSheet([{ range: 'testApi!B1', values: [['=UNIQUE(A1:A3)']]}])
-  // await updateSheet([{ range: 'testApi!B2', values: [['abc']]}])
-  const res = await getSheetValues(['testApi'])
-  console.log(JSON.stringify(res, null, 2))
-  // updateSheet([{
-  //   range: 'testApi!A1',
-  //   values: [['It worked!']]
-  // }])
+  const transactions = await fetchTransactions(transactionsStartDate, transactionsEndDate)
+  const updates = transformTransactionsToUpdates(transactions)
 
-  // await runAudit(auditStartDate, auditEndDate)
-  // let transactions = await getAllTransactions()
-  // console.log(transactions)
+  await updateSheet(updates)
+
+  await runAudit(auditStartDate, auditEndDate)
 })()
